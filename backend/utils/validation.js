953b0a258e5addf11d563113d1ecd8d75dbea0fe
Ludia <lypark1985@gmail.com>
2023-04-14 10:57:58 -0600

    phase 5, completed validation portion

---
+++ backend/utils/validation.js
@@ -0,0 +1,32 @@
+
+const { validationResult } = require('express-validator');
+
+/* 
+middleware for formatting errors from express-validator middleware ↓↓
+calls validationResult from the express-validator package passing in the request. 
+If there are no validation errors returned from the validationResult function, 
+  invoke the next middleware.
+If there are validation errors, create an error with all the validation error messages 
+  and invoke the next error-handling middleware.
+*/
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) { 
+    const errors = {};
+    validationErrors
+      .array()
+      .forEach(error => errors[error.param] = error.msg);
+
+    const err = Error("Bad request.");
+    err.errors = errors;
+    err.status = 400;
+    err.title = "Bad request.";
+    next(err);
+  }
+  next();
+};
+
+module.exports = {
+  handleValidationErrors
+};
