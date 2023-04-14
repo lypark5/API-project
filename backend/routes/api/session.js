46ae2b551a4853018faf9d4d81e445e1456b4a0e
Ludia <lypark1985@gmail.com>
2023-04-14 11:30:08 -0600

    phase 5 validate login request

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,11 +5,37 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
+
+
+
+/* 
+this middleware checks key of credential (either username or email) and key of password.
+this uses 'check' and 'handleValidateErrors' middleware.
+this checks to see whether or not req.body.credential & req.body.password are empty.
+  if one of these is empty, response is error.
+*/
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
+
+
 
 // Log in
 router.post(
   '/',
+  validateLogin,
   async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -81,6 +107,8 @@ router.get(
     } else return res.json({ user: null });
   }
 );
+
+
 
 
 module.exports = router;
