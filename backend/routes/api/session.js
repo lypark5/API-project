fa9ff676b6725c7799a8f62d4e3c64c5462f7bc9
Ludia <lypark1985@gmail.com>
2023-04-14 14:09:45 -0600

    completed Phase 5

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -29,9 +29,6 @@ const validateLogin = [
   handleValidationErrors
 ];
 
-
-
-
 // Log in
 router.post(
   '/',
@@ -58,8 +55,10 @@ router.post(
 
     const safeUser = {
       id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
       email: user.email,
-      username: user.username,
+      username: user.username
     };
 
     await setTokenCookie(res, safeUser);
@@ -98,8 +97,10 @@ router.get(
     if (user) {
       const safeUser = {
         id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email,
-        username: user.username,
+        username: user.username
       };
       return res.json({
         user: safeUser
