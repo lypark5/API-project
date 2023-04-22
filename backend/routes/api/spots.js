5ef471c9453f3aa7149cab26234e01f9333ef889
Ludia <lypark1985@gmail.com>
2023-04-22 16:59:27 -0600

    completed create booking by spotId

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -205,6 +205,13 @@ router.post('/:spotId/bookings', requireAuth, async (req, res, next) => {
   // grab house 4
   let spotById = await Spot.findByPk(req.params.spotId);  // get the specific spot from id.
 
+  // check if this spot id spot exists
+  if (!spotById) {                                  // if the target spot to be deleted doesn't exist
+    let err = new Error("Spot couldn't be found");  // make a relevant error
+    err.status = 404;                               // make error status
+    next(err);                                      // pass along error if this doesn't hit.
+  };
+
   // find all bookings of this property
   let currentBookings = await Booking.findAll({
     where: {spotId: spotById.id} 
@@ -216,26 +223,12 @@ router.post('/:spotId/bookings', requireAuth, async (req, res, next) => {
     bookingsList.push(booking.toJSON())               // this makes each booking object json'ed.
   };
 
-  if (user) {                                         // if someone is logged in,
-    // check if this spot id spot exists
-    if (!spotById) {                                  // if the target spot to be deleted doesn't exist
-      let err = new Error("Spot couldn't be found");  // make a relevant error
-      err.status = 404;                               // make error status
-      next(err);                                      // pass along error if this doesn't hit.
-    };
-
+  // if (user) {                                         // if someone is logged in,
     // error for if you own this spot, can't book it
     if (user.id === spotById.ownerId) {               // if the currently logged-in user(user.id) is not the same as the target property's owner (addPicSpot.ownerId),
       let err = new Error('Forbidden');               // make a new error called forbidden.
       err.status = 403;                               // make error status
       next(err);                                      // pass on error if this doesn't catch.
-    };
-
-    // error for past dates
-    if ((Date.parse(startDate) < Date.now())) {                   // if the currently logged-in user(user.id) is not the same as the target property's owner (addPicSpot.ownerId),
-      let err = new Error("Past bookings can't be modified");     // make a relevant error
-      err.status = 403;                                           // make error status
-      next(err);                                                  // pass along error if this doesn't hit.
     };
 
     // checking for errors and collecting them
@@ -248,42 +241,38 @@ router.post('/:spotId/bookings', requireAuth, async (req, res, next) => {
       return res.status(400).json({message: 'Bad Request', errors: errorObj})       // status code 400, plus "message: Bad Request", plus "errors:" plus the errorObj.
     }
 
-
-
     ///////////////////////
-    // overlapping dates error
+    // overlapping dates error, HARDDDDDD
+    // refer to my drawing to understand
     for (let currentBooking of bookingsList) {
-      if (
-  const { startDate, endDate } = req.body; 
-  vs
-  (Date.parse(startDate) > Date.parse(currentBooking.startDate) && )
-
-
-
-      ) {
-        errorObj.startDate = 'Start date conflicts with an existing booking'          // add startDate key to error obj w/ msg value
-        errorObj.endDate = 'End Date conflicts with an existing booking';                                       
+      // for each current booking, check:
+      // attempted startDates compared to each existing booking dates
+      if (Date.parse(startDate) < Date.parse(currentBooking.endDate)
+        && Date.parse(startDate) > Date.parse(currentBooking.startDate)) {             // if created startDate starts before an existing booking is over
+          errorObj.startDate = 'Start date conflicts with an existing booking';     // add startDate key to error obj w/ msg value
       }
+      if (Date.parse(endDate) > Date.parse(currentBooking.startDate)
+        && Date.parse(endDate) < Date.parse(currentBooking.endDate)) {             // if created endDate is too long, stepping over existing booking start
+          errorObj.endDate = 'End Date conflicts with an existing booking';         // add endDate key to error obj w/ msg value
+      }                                      
+    };
+     ///////////////////////
 
-      if (Object.keys(errorObj).length) {                                             // if the array of all the keys inside errorObj has length > 0
-        return res.status(403).json({message: 'Sorry, this spot is already booked for the specified dates', errors: errorObj})  // status code 400, plus "message: Bad Request", plus "errors:" plus the errorObj.
-      }
-    }
-    ///////////////////
-
-
+    if (Object.keys(errorObj).length) {                                             // if the array of all the keys inside errorObj has length > 0
+      return res.status(403).json({message: 'Sorry, this spot is already booked for the specified dates', errors: errorObj})  // status code 400, plus "message: Bad Request", plus "errors:" plus the errorObj.
+    // }
+    };
     
 
-    // create a new spotImg 
-    let newBooking = await Booking.create({         // variable for new img created
-      spotId: spotById.id,
-      userId: spotById.ownerId,
-      startDate,
-      endDate
-    });  
+  // create a new booking 
+  let newBooking = await Booking.create({         // variable for new img created
+    spotId: spotById.id,
+    userId: spotById.ownerId,
+    startDate,
+    endDate
+  });  
 
-    return res.json(newBooking);
-  }
+  return res.json(newBooking);
 });
 
 
