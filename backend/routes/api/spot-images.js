7e67339acef5bad36f3472f100937fb7d0741e83
Ludia <lypark1985@gmail.com>
2023-04-23 11:33:25 -0600

    completed delete spot img

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -11,6 +11,44 @@ const { requireAuth } = require('../../utils/auth');          // import the midd
 
 
 
+// DELETE A SPOT IMG******************************************************************************
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+  const { user } = req;                                   // destructuring/extracting user key from req
+
+  // find the booking by its id
+  const deletedImg = await SpotImage.findByPk(req.params.imageId);
+
+  // check if this spot img id exists
+  if (!deletedImg) {                                      // if the target spot img to be deleted doesn't exist
+    let err = new Error("Spot Image couldn't be found");  // make a relevant error
+    err.status = 404;                                     // make error status
+    next(err);                                            // pass along error if this doesn't hit.
+  }
+
+  // find this spot
+  const thisSpot = await Spot.findOne({
+    where: {id: deletedImg.spotId}
+  });
+
+  // match up logged-in user id to this spot's owner
+  if (user.id === thisSpot.ownerId) {                   // if the currently logged-in user's id (user.id) === the spot's ownerId,
+    await deletedImg.destroy();                         // destroy the targeted spot img.
+    return res.json({                                   // return the json'ed response:
+      message: 'Successfully deleted'                   // success msg.
+    });
+  } else {                                              // if logged in user is diff to spot owner
+    let err = new Error('Forbidden');                   // make a new error called forbidden.
+    err.status = 403;                                   // make error status
+    next(err);                                          // pass on error if this doesn't catch.
+  };
+});
+
+
+
+
+
+
+
 
 
 
