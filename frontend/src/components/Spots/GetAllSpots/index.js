362a89ca6703b17de145b6da9267e8193d0f2f0f
Ludia <lypark1985@gmail.com>
2023-07-05 00:42:50 -0500

    a lot of homepage stuff

--- frontend/src/components/Spots/GetAllSpots/index.js
+++ frontend/src/components/Spots/GetAllSpots/index.js
@@ -7,13 +7,27 @@ import { GetAllSpotsThunk } from '../../../store/spot';
 function GetAllSpots () {
   const dispatch = useDispatch();
   const spots = useSelector(state => state.spots.allSpots);
+  const spotsArray = Object.values(spots)       // make an obj list into array list
 
   useEffect(() => {
     dispatch(GetAllSpotsThunk());
   }, [dispatch]);
-  console.log('spots =', spots)
+  console.log('spots =', spots)         // spots is an obj of objs
+  console.log('spotsArray =', spotsArray)         
   return (
-    <h1>home~~</h1>
+    <>
+      <h1>home~~</h1>
+      <div>
+        {spotsArray.map(spot => 
+          <div>
+            <img src={spot.previewImage} />
+            <p>{spot.city}, {spot.state}</p>
+            <p>${spot.price} night</p>
+            <p>{spot.avgRating}</p>
+          </div>
+          )}
+      </div>
+    </>
   )
 };
 
